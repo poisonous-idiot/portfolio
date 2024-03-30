fa8842db6ceec833573a1ab564ef9f5a8b51c23d
VardaAli <vardaali282@gmail.com>
2024-03-30 19:58:28 +0500

    fourth commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -113,7 +113,7 @@ const Footer = (props) => {
           </a>
         )}
       </div>
-      <p className="small" style={{ marginTop: 0, color: "white" }}>
+      <p className="small" style={{ marginTop: 0, color: "black" }}>
         Created by {name}
       </p>
     </div>
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -58,7 +58,7 @@ const projectList = [
 const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <h2 style={{ textAlign: "center" }}>Projects</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
