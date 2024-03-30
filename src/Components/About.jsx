31ad30bd270ccc919b8ef621efff1bb54e017fd1
VardaAli <vardaali282@gmail.com>
2024-03-30 20:04:17 +0500

    second commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "..\images\wood-591631_1280.jpg";
+import image from "src\images\wood-591631_1280.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
